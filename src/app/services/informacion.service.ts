a268016993b1aa79f721f7dac4b500827dc4f8f6
Ernesto <ernestoaguaysol@gmail.com>
2017-10-30 17:26:37 -0300

    servicio de firebase json

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,15 +5,37 @@ import { Http } from '@angular/http';
 export class InformacionService {
 
   info:any = {};
-  cargada:boolean = false;
+  cargada_info:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+  equipo:any[] = [];
 
   constructor( public http: Http ) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  /**
+   * carga_info
+   */
+  public carga_info() {
     this.http.get('assets/data/info.pagina.json')
-        .subscribe(data => {
-          console.log(data.json());
-          this.cargada = true;
-          this.info = data.json();
-        });
+    .subscribe(data => {
+      // console.log(data.json());
+      this.cargada_info = true;
+      this.info = data.json();
+    });
+  }
+
+  /**
+   * carga_sobre_nosotros
+   */
+  public carga_sobre_nosotros() {
+    this.http.get('https://paginaweb-34085.firebaseio.com/equipo.json')
+    .subscribe(data => {
+      console.log(data.json());
+      this.cargada_sobre_nosotros = true;
+      this.equipo = data.json();
+    });
   }
 
 }
